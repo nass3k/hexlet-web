1ed0cafc4873aa65af43cc157537daa3f966479f
n4ssek <nass3k@gmail.com>
2019-11-11 12:26:47 +0300

    files added

--- App/public/index.php
+++ App/public/index.php
@@ -1,30 +1,13 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
-use DI\Container;
 
-$users = App\Generator::generate(100);
-
-$container = new Container();
-$container->set('renderer', function () {
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
-});
-
-AppFactory::setContainer($container);
 $app = AppFactory::create();
+$app->setBasePath("/public");
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $this->get('renderer')->render($response, 'index.phtml');
+    return $response->write('Welcome to Slim!');
 });
-
-$app->get('/users/{id}', function ($request, $response, $args) {
-    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
-    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
-    // $this доступен внутри анонимной функции благодаря http://php.net/manual/ru/closure.bindto.php
-    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
-});
-
 $app->run();
