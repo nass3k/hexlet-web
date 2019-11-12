087e59e8cff96e6b0d5c14f5ec06789d78ae2883
n4ssek <nass3k@gmail.com>
2019-11-13 00:09:23 +0300

    files added

--- App/public/index.php
+++ App/public/index.php
@@ -1,24 +1,2 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use Slim\Factory\AppFactory;
-use DI\Container;
-
-$users = App\Generator::generate(100);
-
-$container = new Container();
-$container->set('renderer', function () {
-    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
-});
-
-AppFactory::setContainer($container);
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-
-$app->get('/', function ($request, $response) {
-    return $this->get('renderer')->render($response, 'index.phtml');
-});
-
-
-$app->run();
---
+++ completed/template/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use DI\Container;
+
+$users = App\Generator::generate(100);
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $this->get('renderer')->render($response, 'index.phtml');
+});
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $params = [
+        'users' => $users
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $user = collect($users)->firstWhere('id', $args['id']);
+    $params = [
+        'id' => $args['id'],
+        'user' => $user
+    ];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+$app->run();
